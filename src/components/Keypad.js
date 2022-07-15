f915bb79872665a8e8390c730aec1bf2afad446f
Leestan360 <stanleymayore2@gmail.com>
2022-07-15 10:17:36 +0300

    Done.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
 // Code Keypad Component Here
+import React from "react";
+
+function Keypad() {
+  function handleKeypad(e) {
+    // e.preventDefault();
+    console.log("Entering password...");
+  }
+
+  return <input type="password" onChange={handleKeypad} />
+}
+
+export default Keypad;
